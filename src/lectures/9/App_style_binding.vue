bb2345d1925c59d3131a02ceaf4c2d20b74ff929
sooyong.song <sooyong.song@appg.co.kr>
2023-04-05 11:02:27 +0900

    style bindings

---
+++ src/lectures/9/App_style_binding.vue
@@ -0,0 +1,54 @@
+<template>
+	<div class="text" :class="[activeClass, errorClass]">This is text.</div>
+	<button v-on:click="toggle">toggle</button>
+	<button v-on:click="hasError = !hasError">toggleError</button>
+</template>
+
+<script>
+import { ref, computed } from 'vue';
+
+export default {
+	setup() {
+		const isActive = ref(true);
+		const hasError = ref(false);
+
+		// const classObject = reactive({
+		// 	active: true,
+		// 	'text-danger': true,
+		// });
+
+		const classObject = computed(() => {
+			return {
+				active: true && true,
+				'text-danger': true && true,
+			};
+		});
+
+		const activeClass = ref('active');
+		const errorClass = ref('error');
+
+		const toggle = () => {
+			isActive.value = !isActive.value;
+		};
+
+		return {
+			isActive,
+			toggle,
+			hasError,
+			classObject,
+			activeClass,
+			errorClass,
+		};
+	},
+};
+</script>
+
+<style scoped>
+.active {
+	font-weight: 900;
+}
+
+.text-danger {
+	color: red;
+}
+</style>
