e35c8eb57049cfcb829bf40d91e6c1073ac30bee
chris1797 <jaehun6004@gmail.com>
2023-04-10 15:03:06 +0900

    props

---
+++ src/components/TheView.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>
+    <div class="container py-4">
+      <div class="row g-5">
+        <div class="col col-4"></div>
+        <div class="col col-4"></div>
+        <div class="col col-4"></div>
+        <div class="col col-4"></div>
+        <div class="col col-4"></div>
+        <div class="col col-4"></div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
