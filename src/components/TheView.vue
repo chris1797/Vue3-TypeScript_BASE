38a7a61aa23c5bcfcd5f01391077364513826dc8
chris1797 <jaehun6004@gmail.com>
2023-04-13 11:17:33 +0900

    Lifecycle Hook - Updating

--- src/components/TheView.vue
+++ src/components/TheView.vue
@@ -1,17 +1,14 @@
 <template>
   <main>
-    <div class="container py-4">
-      <ProvideInject></ProvideInject>
-    </div>
+    <LifecycleHooks></LifecycleHooks>
   </main>
 </template>
 
 <script>
-import ProvideInject from './ProvideInject.vue';
-
+import LifecycleHooks from './LifecycleHooks.vue';
 export default {
   components: {
-    ProvideInject,
+    LifecycleHooks,
   },
   setup() {
     return {};
