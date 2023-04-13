4ce43ded8b77636a718f5759181fd6350d3c740d
chris1797 <jaehun6004@gmail.com>
2023-04-13 11:57:31 +0900

    Lifecycle Unmount

--- src/components/LifecycleChild.vue
+++ src/components/LifecycleChild.vue
@@ -1,18 +1,31 @@
 <template>
-  <div></div>
+  <div class="card">
+    <div class="card-body">LifecycleChild</div>
+    <input id="input" type="text" />
+  </div>
 </template>
 
 <script lang="ts">
-import { onBeforeMount, onMounted } from 'vue';
+import { onBeforeMount, onMounted, onBeforeUnmount, onUnmounted } from 'vue';
 
 export default {
   setup() {
-    console.log('[Child] setup');
+    // console.log('[Child] setup');
     onBeforeMount(() => {
       console.log('[Child] onBeforeMount');
     });
     onMounted(() => {
       console.log('[Child] onMounted');
+    });
+    onBeforeUnmount(() => {
+      console.log('[Child] onBeforeUnmount');
+      console.log(document.querySelector('#input')); // <input id="input" type="text" /> DOM 출력
+    });
+
+    // mount를 해제한 이후이기 때문에 DOM을 가져오면 없음
+    onUnmounted(() => {
+      console.log('[Child] onUnmounted');
+      console.log(document.querySelector('#input')); // 아무것도 안찍힘
     });
     return {};
   },
--- src/components/LifecycleHooks.vue
+++ src/components/LifecycleHooks.vue
@@ -1,23 +1,26 @@
 <template>
   <div class="container py-4">
     <input ref="inputRef" type="text" value="hello world" />
-    <!-- <LifecycleChild></LifecycleChild> -->
+    <hr />
+    <button @click="visible = !visible">Toggle Child</button>
+    <LifecycleChild v-if="visible"></LifecycleChild>
     <p id="message">{{ message }}</p>
   </div>
 </template>
 
 <script lang="ts">
 import { onBeforeMount, ref, onMounted, onBeforeUpdate, onUpdated } from 'vue';
-// import LifecycleChild from './LifecycleChild.vue';
+import LifecycleChild from './LifecycleChild.vue';
 
 export default {
   components: {
-    // LifecycleChild,
+    LifecycleChild,
   },
   setup() {
     console.log('setup');
     const inputRef = ref(null);
     const message = ref('');
+    const visible = ref(false);
     // onBeforeMount(() => {
     //   console.log('onBeforeMount');
     // });
@@ -29,26 +32,26 @@ export default {
     message.value 값은 찍히지만 DOM이 업데이트 되기 전이므로
     DOM Content는 찍히지 않음
     */
-    onBeforeUpdate(() => {
-      console.log('onBeforeUpdate: ', message.value);
-      console.log(
-        'DOM Content: ',
-        document.querySelector('#message')?.textContent,
-      );
-    });
+    // onBeforeUpdate(() => {
+    //   console.log('onBeforeUpdate: ', message.value);
+    //   console.log(
+    //     'DOM Content: ',
+    //     document.querySelector('#message')?.textContent,
+    //   );
+    // });
 
     /*
     setup()에서 return된 데이터가 변경되었을 경우에 실행됨
     -> DOM이 업데이트 될 때
     */
-    onUpdated(() => {
-      console.log('onUpdated: ', message.value);
-      console.log(
-        'DOM Content: ',
-        document.querySelector('#message')?.textContent,
-      );
-    });
-    return { inputRef, message };
+    // onUpdated(() => {
+    //   console.log('onUpdated: ', message.value);
+    //   console.log(
+    //     'DOM Content: ',
+    //     document.querySelector('#message')?.textContent,
+    //   );
+    // });
+    return { inputRef, message, visible };
   },
 };
 </script>
