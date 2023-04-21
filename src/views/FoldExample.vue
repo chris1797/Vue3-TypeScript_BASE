739941e8401be8c1329e217c0b961e48f7aacc22
chris1797 <jaehun6004@gmail.com>
2023-04-21 14:44:03 +0900

    FAQ folding

--- src/views/FoldExample.vue
+++ src/views/FoldExample.vue
@@ -1,24 +1,61 @@
 <template>
   <div>
+    <br />
     <h1>My Website</h1>
-    <nav v-show="showNav">
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-    </nav>
-    <button @click="toggleNav">Toggle Navigation</button>
+    <br />
+
+    <div class="faq">
+      <div v-for="faq in faqList.slice().reverse()" :key="faq">
+        <li class="faq-title">{{ faq.title }}</li>
+        <button
+          class="navbar-toggler"
+          v-if="!faq.isVisible"
+          @click.prevent="toggleContent(faq.faq_idx)"
+        >
+          ▼
+        </button>
+        <button
+          class="navbar-toggler"
+          v-else
+          @click.prevent="toggleContent(faq.faq_idx)"
+        >
+          ▲
+        </button>
+        <div class="faq-content" v-show="faq.isVisible">
+          {{ faq.content }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
 
-const showNav = ref(false);
+const faqList = ref([
+  {
+    faq_idx: 0,
+    title: '자주 묻는 질문 1',
+    content: '123111',
+    isVisible: false,
+  },
+  {
+    faq_idx: 1,
+    title: '자주 묻는 질문 2',
+    content: '321',
+    isVisible: false,
+  },
+  {
+    faq_idx: 2,
+    title: '자주 묻는 질문 3',
+    content: 'ara',
+    isVisible: false,
+  },
+]);
 
-function toggleNav() {
-  showNav.value = !showNav.value;
+function toggleContent(index) {
+  // console.log('index: ', index, this.faqList[index].isVisible);
+  this.faqList[index].isVisible = !this.faqList[index].isVisible;
 }
 </script>
 
@@ -27,5 +64,19 @@ function toggleNav() {
   nav {
     display: none;
   }
+}
+
+.faq {
+  padding-left: 2rem;
+  margin: 0 auto;
+}
+
+.faq-title {
+  float: left;
+  width: 80%;
+}
+
+.foldbutton {
+  float: left;
 }
 </style>
